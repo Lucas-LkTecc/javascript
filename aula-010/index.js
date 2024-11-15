b4c5bde5496f42d2a191e73209f7d44dd84062cf
Lucas-LkTecc <ruan.alencar97@gmail.com>
2024-11-15 11:10:14 -0300

    aulaudemy

---
+++ aula-010/index.js
@@ -0,0 +1,18 @@
+//Tipos de dados primitivos
+// string(Texto), number(numero), undefined(não atribuido), null(nulo), boolean(Valor lógico(sim ou nao)).
+const nome = 'Luiz'; //string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+
+const num1 = 10; // number 
+const num2 = 10.52; // number (10.52 numero com ponto flutuante)
+
+let nomeAluno; // undefined --> não aponta para local nenhum na memória.
+let sobrenomeAluno = null; // nulo --> não aponta para local nenhum na memória a diferença é que  só utilizamos o NULL quando queremos desconfigurar uma varial.
+
+const boolean = true; // boolean = true ou false (é utilizado quando queremos mudar o fluxo da aplicação).
+
+console.log(typeof nome); // utiliazamos typeof quando queremos verificar o tipo das variaveis.
+
+
+
