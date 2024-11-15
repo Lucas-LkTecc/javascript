46b4e95c7816db4a0e5b7a7c7ba6f9844f9f85b6
Lucas-LkTecc <ruan.alencar97@gmail.com>
2024-11-15 14:25:12 -0300

    aulaudemy

---
+++ aula-011/index.js
@@ -0,0 +1,26 @@
+// Operadores aritiméticos, de Atribuição e Incremento
+
+/*
+-Aritiméticos
+
+Adição e Concatenação (+)
+divisão (/)
+Subtração (-)
+Multiplicação (*)
+Potenciação (**)
+Resto da divisão (%)
+Incremento (++)
+Decremento (--)
+
+Operadores de atribuição:
++= 
+*=
+-=
+/=
+**=
+
+NaN= Not a number 
+parseInt (inteiro)
+parseFloat (decimais)
+*/
+
